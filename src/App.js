84502203f1873876e27eb8a60fa37e0bf0236ca3
ngraton <mail@ngraton.dev>
2020-04-11 13:12:19 -0400

    :alembic: paper texture background

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
     </div>
   );
 }
