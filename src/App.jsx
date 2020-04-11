796d0d8e43af73d61df968afb1fbb85969129f26
ngraton <mail@ngraton.dev>
2020-04-11 16:34:20 -0400

    :art: book component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import titleImg from './img/peter-title.png'
+import Book from './components/Book.jsx'
 
 function App() {
   return (
     <div className="App">
-      <h2>THE TALE OF PETER RABBIT</h2>
-      <h4>BY BEATRIX POTTER</h4>
-      <img src={titleImg} alt=""/>
+      <Book />
     </div>
   );
 }
---
+++ src/components/Book.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import titleImg from './img/peter-title.png'
+
+
+function Book() {
+  return (
+    <div>
+      <h2>THE TALE OF PETER RABBIT</h2>
+      <h4>BY BEATRIX POTTER</h4>
+      <img src={titleImg} alt=""/>
+    </div>
+  )
+}
