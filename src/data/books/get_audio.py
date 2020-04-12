1257e484ea882ad7af111d428e24095c14d21484
ngraton <mail@ngraton.dev>
2020-04-12 00:50:53 -0400

    :alembic: get all audio

--- src/data/books/get_audio.py
+++ src/data/books/get_audio.py
@@ -5,41 +5,45 @@ from contextlib import closing
 import os
 import sys
 import subprocess
+import json
 from tempfile import gettempdir
 
-# Create a client using the credentials and region defined in the [adminuser]
-# section of the AWS credentials file (~/.aws/credentials).
-session = Session()
-polly = session.client("polly")
+text_list = json.loads(open("./TheTaleOfPeterRabbit.json", "r"))
 
-try:
-    # Request speech synthesis
-    response = polly.synthesize_speech(Text="Once upon a time there were four little Rabbits, and their names were—\n\t\t\tFlopsy,\n\t\tMopsy,\n\tCotton-tail,\nand Peter.\n\nThey lived with their Mother in a sand-bank, underneath the root of a very big fir-tree.", OutputFormat="mp3",
-                                        VoiceId="Amy")
-except (BotoCoreError, ClientError) as error:
-    # The service returned an error, exit gracefully
-    print(error)
-    sys.exit(-1)
+for text, index in text_list:
+    # Create a client using the credentials and region defined in the [adminuser]
+    # section of the AWS credentials file (~/.aws/credentials).
+    session = Session()
+    polly = session.client("polly")
 
-# Access the audio stream from the response
-if "AudioStream" in response:
-    # Note: Closing the stream is important because the service throttles on the
-    # number of parallel connections. Here we are using contextlib.closing to
-    # ensure the close method of the stream object will be called automatically
-    # at the end of the with statement's scope.
-    with closing(response["AudioStream"]) as stream:
-        # output = os.path.join(gettempdir(), "speech.mp3")
+    try:
+        # Request speech synthesis
+        response = polly.synthesize_speech(Engine="neural", Text=text, OutputFormat="mp3",
+                                            VoiceId="Amy")
+    except (BotoCoreError, ClientError) as error:
+        # The service returned an error, exit gracefully
+        print(error)
+        sys.exit(-1)
 
-        try:
-            # Open a file for writing the output as a binary stream
-            with open("speech.mp3", "wb") as file:
-                file.write(stream.read())
-        except IOError as error:
-            # Could not write to file, exit gracefully
-            print(error)
-            sys.exit(-1)
+    # Access the audio stream from the response
+    if "AudioStream" in response:
+        # Note: Closing the stream is important because the service throttles on the
+        # number of parallel connections. Here we are using contextlib.closing to
+        # ensure the close method of the stream object will be called automatically
+        # at the end of the with statement's scope.
+        with closing(response["AudioStream"]) as stream:
+            # output = os.path.join(gettempdir(), "speech.mp3")
 
-else:
-    # The response didn't contain audio data, exit gracefully
-    print("Could not stream audio")
-    sys.exit(-1)
+            try:
+                # Open a file for writing the output as a binary stream
+                with open("{index}.mp3", "wb") as file:
+                    file.write(stream.read())
+            except IOError as error:
+                # Could not write to file, exit gracefully
+                print(error)
+                sys.exit(-1)
+
+    else:
+        # The response didn't contain audio data, exit gracefully
+        print("Could not stream audio")
+        sys.exit(-1)
