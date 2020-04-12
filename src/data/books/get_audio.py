f4d1beec0c4624eb5575dfe90221db1bc8d32915
ngraton <mail@ngraton.dev>
2020-04-12 00:25:15 -0400

    :alembic: auido

---
+++ src/data/books/get_audio.py
@@ -0,0 +1,45 @@
+"""Getting Started Example for Python 2.7+/3.3+"""
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import os
+import sys
+import subprocess
+from tempfile import gettempdir
+
+# Create a client using the credentials and region defined in the [adminuser]
+# section of the AWS credentials file (~/.aws/credentials).
+session = Session(profile_name="adminuser")
+polly = session.client("polly")
+
+try:
+    # Request speech synthesis
+    response = polly.synthesize_speech(Text="Once upon a time there were four little Rabbits, and their names were—\n\t\t\tFlopsy,\n\t\tMopsy,\n\tCotton-tail,\nand Peter.\n\nThey lived with their Mother in a sand-bank, underneath the root of a very big fir-tree.", OutputFormat="mp3",
+                                        VoiceId="Amy")
+except (BotoCoreError, ClientError) as error:
+    # The service returned an error, exit gracefully
+    print(error)
+    sys.exit(-1)
+
+# Access the audio stream from the response
+if "AudioStream" in response:
+    # Note: Closing the stream is important because the service throttles on the
+    # number of parallel connections. Here we are using contextlib.closing to
+    # ensure the close method of the stream object will be called automatically
+    # at the end of the with statement's scope.
+    # with closing(response["AudioStream"]) as stream:
+        output = os.path.join(gettempdir(), "speech.mp3")
+
+        try:
+            # Open a file for writing the output as a binary stream
+            # with open(output, "wb") as file:
+                file.write(stream.read())
+        except IOError as error:
+            # Could not write to file, exit gracefully
+            print(error)
+            sys.exit(-1)
+
+else:
+    # The response didn't contain audio data, exit gracefully
+    print("Could not stream audio")
+    sys.exit(-1)
